e9899d736c38c3cae3abb1b48b9330a534c6194e
Adrien Pichard <adrien.pichard@epitech.eu>
2019-05-01 16:42:29 +0200

    Coding style

--- src/command.c
+++ src/command.c
@@ -11,8 +11,8 @@
 int command(vec_t *command, shell_t *shell)
 {
     char *builtin[] = {"env", "setenv", "unsetenv", "exit", "cd", "echo", NULL};
-    int (*function[])(vec_t *params, shell_t *status) = {my_env, my_setenv, my_unsetenv, my_exit, my_cd,
-    my_echo, NULL};
+    int (*function[])(vec_t *params, shell_t *status) = {my_env, my_setenv,
+        my_unsetenv, my_exit, my_cd, my_echo, NULL};
     int n = index_of_str((char *)(command->content[0]), builtin);
 
     if (n != -1) {
