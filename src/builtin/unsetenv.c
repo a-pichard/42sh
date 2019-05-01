c152d8911245ae281d95bf44585b4ff2061d8340
thomas roux <thomasroux021@gmail.com>
2019-05-01 15:07:45 +0200

    fix status ensetenv to shell status

--- src/builtin/unsetenv.c
+++ src/builtin/unsetenv.c
@@ -17,13 +17,16 @@ int my_unsetenv(vec_t *params, shell_t *shell)
 
     if (params->element == 1) {
         my_puterr("unsetenv: Too few arguments.\n");
+        shell->status = 1;
         return (1);
     }
     for (int i = 0; i != params->element && status != -1; i += 1)
         status = unsetenv((char *)params->content[i]);
     if (status == -1) {
         perror("unsetenv:");
+        shell->status = 1;
         return (1);
     }
+    shell->status = 0;
     return (0);
 }
