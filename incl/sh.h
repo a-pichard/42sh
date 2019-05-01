f878edda621e32449fd9e2e6974ad1186c92cfd0
armand <armandpicard71@gmail.com>
2019-05-01 16:10:30 +0200

    add simple prompt to exec builtin

--- incl/sh.h
+++ incl/sh.h
@@ -8,7 +8,7 @@
 #ifndef SH_H_
 #define SH_H_
 #include "vec.h"
-
+#define SEPARATOR {";", "|", ">>", "<<", ">", "<", NULL}
 extern char **environ;
 
 void my_putchar(char c);
@@ -21,6 +21,16 @@ char **tabdup(char **env);
 void *xmalloc(int);
 int index_of(char c, char *tab);
 int index_of_str(char *str, char **tab);
+vec_t *my_str_to_word_tab_plus(char *str, char *separators,
+    char **separator_and_word);
+
+struct shell_s
+{
+    int status;
+};
+typedef struct shell_s shell_t;
+
+int command(vec_t *command, shell_t *shell);
 
 //builtin
 int my_cd(vec_t *params, shell_t *shell);
@@ -30,11 +40,6 @@ int my_unsetenv(vec_t *params, shell_t *shell);
 int my_exit(vec_t *params, shell_t *shell);
 int my_echo(vec_t *params, shell_t *shell);
 
-struct shell_s
-{
-    int status;
-};
 
-typedef struct shell_s shell_t;
 
 #endif
---
+++ src/command.c
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2019
+** command
+** File description:
+** command
+*/
+
+#include "vec.h"
+#include "sh.h"
+
+int command(vec_t *command, shell_t *shell)
+{
+    char *builtin[] = {"env", "setenv", "unsetenv", "exit", "cd", "echo", NULL};
+    void (*function[])(vec_t *params, shell_t *status) = {my_env, my_setenv, my_unsetenv, my_exit, my_cd,
+    my_echo, NULL};
+    int n = index_of_str((char *)(command->content[0]), builtin);
+
+    if (n != -1) {
+        (function[n])(command, shell);
+    } else {
+        // try to execute a program
+    }
+}
