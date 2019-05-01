a104fc019d3c8f8b4da8eadd26e0c7686da31898
Adrien Pichard <adrien.pichard@epitech.eu>
2019-05-01 16:05:58 +0200

    Coding style

--- src/builtin/exit.c
+++ src/builtin/exit.c
@@ -14,12 +14,9 @@
 int my_exit(vec_t *params, shell_t *shell)
 {
     my_putstr("exit\n");
-    if (params->element == 2) {
-        shell->status = 0;
+    if (params->element == 2)
         exit(atoi(params->content[1]));
-    } else {
-        shell->status = 0;
+    else
         exit(shell->status);
-    }
     return (0);
 }
