9677cd35c2dce50147b9c882f34d14beababaf71
armand <armandpicard71@gmail.com>
2019-05-07 12:13:44 +0200

    add execvp

--- src/command.c
+++ src/command.c
@@ -7,6 +7,9 @@
 
 #include "vec.h"
 #include "sh.h"
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 int command(vec_t *command, shell_t *shell)
 {
@@ -14,10 +17,22 @@ int command(vec_t *command, shell_t *shell)
     int (*function[])(vec_t *params, shell_t *status) = {my_env, my_setenv,
         my_unsetenv, my_exit, my_cd, my_echo, NULL};
     int n = index_of_str((char *)(command->content[0]), builtin);
+    int pid = 0;
 
     if (n != -1) {
         (function[n])(command, shell);
     } else {
-        // try to execute a program
+        pid = fork();
+        if (pid == -1) {
+            my_puterr("fork error\n");
+            return (-1);
+        } else if (!pid) {
+            execvp(command->content[0], (char * const *)command->content);
+            my_puterr("execvp a fail\n");
+            exit(84);
+        } else {
+            return (pid);
+        }
     }
+    return (0);
 }
